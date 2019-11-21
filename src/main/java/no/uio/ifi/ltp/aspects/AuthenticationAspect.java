b11f0c72da7c4e8da25b6ff914af8ce47ba00c50
dtitov <dmytro.titov@gmail.com>
2019-11-21 15:13:45 +0100

    Refactor AuthenticationAspect

--- src/main/java/no/uio/ifi/ltp/aspects/AuthenticationAspect.java
+++ src/main/java/no/uio/ifi/ltp/aspects/AuthenticationAspect.java
@@ -6,15 +6,11 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
 
 import javax.servlet.http.HttpServletRequest;
-import java.nio.charset.Charset;
-import java.util.Arrays;
 import java.util.UUID;
 
 @Slf4j
@@ -23,16 +19,13 @@ import java.util.UUID;
 public class AuthenticationAspect {
 
     @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
     private AuthenticationService authenticationService;
 
-    @Around("@annotation(org.springframework.web.bind.annotation.RequestMapping) && args(javax.servlet.http.HttpServletRequest, ..)")
+    @Around("execution(public * no.uio.ifi.ltp.rest.ProxyController.*(..))")
     public Object authenticate(ProceedingJoinPoint joinPoint) throws Throwable {
-        HttpClientErrorException unauthorizedException = HttpClientErrorException.Unauthorized.create(HttpStatus.UNAUTHORIZED, "Unauthorized", HttpHeaders.EMPTY, null, Charset.defaultCharset());
-        HttpServletRequest request = Arrays.stream(joinPoint.getArgs())
-                .filter(a -> a instanceof HttpServletRequest)
-                .findAny()
-                .map(r -> (HttpServletRequest) r)
-                .orElseThrow(() -> unauthorizedException);
         try {
             authenticationService.authenticate(request);
             return joinPoint.proceed();
--- src/main/java/no/uio/ifi/ltp/rest/ProxyController.java
+++ src/main/java/no/uio/ifi/ltp/rest/ProxyController.java
@@ -1,17 +1,16 @@
 package no.uio.ifi.ltp.rest;
 
+import kong.unirest.json.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import no.uio.ifi.tc.TSDFileAPIClient;
 import no.uio.ifi.tc.TokenType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 @Slf4j
 @RestController("/")
@@ -22,10 +21,17 @@ public class ProxyController {
 
     @PutMapping("/upload/{fileName}")
     @ResponseBody
-    public ResponseEntity upload(HttpServletRequest request,
-                                 @PathVariable("fileName") String fileName) throws IOException {
+    public ResponseEntity upload(InputStream inputStream, @PathVariable("fileName") String fileName) throws IOException {
         String token = tsdFileAPIClient.getToken(TokenType.IMPORT);
-        String result = tsdFileAPIClient.upload(token, request.getInputStream(), fileName);
+        String result = tsdFileAPIClient.upload(token, inputStream, fileName);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/resumables")
+    @ResponseBody
+    public ResponseEntity resumables() {
+        String token = tsdFileAPIClient.getToken(TokenType.IMPORT);
+        List<JSONObject> result = tsdFileAPIClient.getResumableUploads(token);
         return ResponseEntity.ok(result);
     }
 
