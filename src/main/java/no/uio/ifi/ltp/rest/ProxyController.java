654fd9020f48e5c01ed0f790a956204b465323c1
dtitov <dmytro.titov@gmail.com>
2019-11-25 11:06:19 +0100

    Refactor ProxyController

--- src/main/java/no/uio/ifi/ltp/rest/ProxyController.java
+++ src/main/java/no/uio/ifi/ltp/rest/ProxyController.java
@@ -1,16 +1,17 @@
 package no.uio.ifi.ltp.rest;
 
-import kong.unirest.json.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import no.uio.ifi.tc.TSDFileAPIClient;
-import no.uio.ifi.tc.TokenType;
+import no.uio.ifi.tc.model.TokenType;
+import no.uio.ifi.tc.model.pojo.Chunk;
+import no.uio.ifi.tc.model.pojo.Token;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
 
 @Slf4j
 @RestController("/")
@@ -21,18 +22,42 @@ public class ProxyController {
 
     @PutMapping("/upload/{fileName}")
     @ResponseBody
-    public ResponseEntity upload(InputStream inputStream, @PathVariable("fileName") String fileName) throws IOException {
-        String token = tsdFileAPIClient.getToken(TokenType.IMPORT);
-        String result = tsdFileAPIClient.upload(token, inputStream, fileName);
+    public ResponseEntity upload(InputStream inputStream,
+                                 @PathVariable("fileName") String fileName) throws IOException {
+        Token token = tsdFileAPIClient.getToken(TokenType.IMPORT);
+        return ResponseEntity.ok(tsdFileAPIClient.upload(token.getToken(), inputStream, fileName));
+    }
+
+    @PatchMapping("/stream/{fileName}")
+    @ResponseBody
+    public ResponseEntity stream(InputStream inputStream,
+                                 @PathVariable("fileName") String fileName,
+                                 @RequestParam(value = "chunk", required = false) String chunk,
+                                 @RequestParam(value = "uploadId", required = false) String uploadId) throws IOException {
+        Token token = tsdFileAPIClient.getToken(TokenType.IMPORT);
+        Chunk result;
+        if (StringUtils.isEmpty(uploadId)) { // new upload
+            result = tsdFileAPIClient.initializeResumableUpload(token.getToken(), inputStream.readAllBytes(), fileName);
+        } else if ("end".equalsIgnoreCase(chunk)) { // finalizing upload
+            result = tsdFileAPIClient.finalizeResumableUpload(token.getToken(), uploadId);
+        } else { // uploading an intermediate chunk
+            result = tsdFileAPIClient.uploadChunk(token.getToken(), Long.parseLong(chunk), inputStream.readAllBytes(), uploadId);
+        }
         return ResponseEntity.ok(result);
     }
 
     @GetMapping("/resumables")
     @ResponseBody
-    public ResponseEntity resumables() {
-        String token = tsdFileAPIClient.getToken(TokenType.IMPORT);
-        List<JSONObject> result = tsdFileAPIClient.getResumableUploads(token);
-        return ResponseEntity.ok(result);
+    public ResponseEntity reumables() {
+        Token token = tsdFileAPIClient.getToken(TokenType.IMPORT);
+        return ResponseEntity.ok(tsdFileAPIClient.getResumableUploads(token.getToken()));
+    }
+
+    @DeleteMapping("/resumables")
+    @ResponseBody
+    public ResponseEntity reumables(@RequestParam(value = "uploadId", required = false) String uploadId) {
+        Token token = tsdFileAPIClient.getToken(TokenType.IMPORT);
+        return ResponseEntity.ok(tsdFileAPIClient.deleteResumableUpload(token.getToken(), uploadId));
     }
 
 }
