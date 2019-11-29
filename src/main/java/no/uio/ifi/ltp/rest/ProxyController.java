b0ccc699281aa0239909a0763c27676d42264f01
dtitov <dmytro.titov@gmail.com>
2019-11-29 16:43:47 +0100

    Add MD5s

--- src/main/java/no/uio/ifi/ltp/rest/ProxyController.java
+++ src/main/java/no/uio/ifi/ltp/rest/ProxyController.java
@@ -4,60 +4,73 @@ import lombok.extern.slf4j.Slf4j;
 import no.uio.ifi.tc.TSDFileAPIClient;
 import no.uio.ifi.tc.model.TokenType;
 import no.uio.ifi.tc.model.pojo.Chunk;
+import no.uio.ifi.tc.model.pojo.ResumableUpload;
 import no.uio.ifi.tc.model.pojo.Token;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.io.InputStream;
 
-import static no.uio.ifi.ltp.auth.impl.JWTAuthenticationService.ELIXIR_IDENTITY;
-
+@SuppressWarnings("ALL")
 @Slf4j
 @RestController
 public class ProxyController {
 
     @Autowired
-    private HttpServletRequest request;
-
-    @Autowired
     private TSDFileAPIClient tsdFileAPIClient;
-
-    @PutMapping("/upload/{fileName}")
-    public ResponseEntity upload(InputStream inputStream,
-                                 @PathVariable("fileName") String fileName) throws IOException {
-        Token token = tsdFileAPIClient.getToken(TokenType.IMPORT);
-        return ResponseEntity.ok(tsdFileAPIClient.upload(token.getToken(), inputStream, fileName));
-    }
 
     @PatchMapping("/stream/{fileName}")
     public ResponseEntity stream(InputStream inputStream,
                                  @PathVariable("fileName") String fileName,
+                                 @RequestParam(value = "uploadId", required = false) String uploadId,
                                  @RequestParam(value = "chunk", required = false) String chunk,
-                                 @RequestParam(value = "uploadId", required = false) String uploadId) throws IOException {
+                                 @RequestParam(value = "md5") String md5) throws IOException {
         Token token = tsdFileAPIClient.getToken(TokenType.IMPORT);
-        Chunk result;
-        if (StringUtils.isEmpty(uploadId)) { // new upload
-            result = tsdFileAPIClient.initializeResumableUpload(token.getToken(), inputStream.readAllBytes(), fileName);
-        } else if ("end".equalsIgnoreCase(chunk)) { // finalizing upload
-            result = tsdFileAPIClient.finalizeResumableUpload(token.getToken(), uploadId);
-        } else { // uploading an intermediate chunk
-            result = tsdFileAPIClient.uploadChunk(token.getToken(), Long.parseLong(chunk), inputStream.readAllBytes(), uploadId);
+
+        byte[] chunkBytes = inputStream.readAllBytes();
+
+        // new upload
+        if (StringUtils.isEmpty(uploadId)) {
+            Chunk response = tsdFileAPIClient.initializeResumableUpload(token.getToken(), chunkBytes, fileName);
+            ResumableUpload resumableUpload = tsdFileAPIClient.getResumableUpload(token.getToken(), response.getId()).orElseThrow();
+            if (!md5.equalsIgnoreCase(resumableUpload.getMd5Sum())) {
+                tsdFileAPIClient.deleteResumableUpload(token.getToken(), response.getId());
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Checksum mismatch. Resumable upload interrrupted and can't be resumed. Please, re-upload the whole file.");
+            }
+            return ResponseEntity.ok(chunk);
         }
-        return ResponseEntity.ok(result);
+
+        // finalizing upload
+        if ("end".equalsIgnoreCase(chunk)) {
+            return ResponseEntity.ok(tsdFileAPIClient.finalizeResumableUpload(token.getToken(), uploadId));
+        }
+
+        // uploading an intermediate chunk
+        Chunk response = tsdFileAPIClient.uploadChunk(token.getToken(), Long.parseLong(chunk), chunkBytes, uploadId);
+        ResumableUpload resumableUpload = tsdFileAPIClient.getResumableUpload(token.getToken(), response.getId()).orElseThrow();
+        if (!md5.equalsIgnoreCase(resumableUpload.getMd5Sum())) {
+            tsdFileAPIClient.deleteResumableUpload(token.getToken(), response.getId());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Checksum mismatch. Resumable upload interrrupted and can't be resumed. Please, re-upload the whole file.");
+        }
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/resumables")
-    public ResponseEntity resumables() {
+    public ResponseEntity getResumables(@RequestParam(value = "uploadId", required = false) String uploadId) {
         Token token = tsdFileAPIClient.getToken(TokenType.IMPORT);
-        return ResponseEntity.ok(tsdFileAPIClient.getResumableUploads(token.getToken()));
+        if (StringUtils.isEmpty(uploadId)) {
+            return ResponseEntity.ok(tsdFileAPIClient.getResumableUploads(token.getToken()));
+        } else {
+            return ResponseEntity.ok(tsdFileAPIClient.getResumableUpload(token.getToken(), uploadId));
+        }
     }
 
     @DeleteMapping("/resumables")
-    public ResponseEntity resumables(@RequestParam(value = "uploadId") String uploadId) {
+    public ResponseEntity deleteResumable(@RequestParam(value = "uploadId") String uploadId) {
         Token token = tsdFileAPIClient.getToken(TokenType.IMPORT);
         return ResponseEntity.ok(tsdFileAPIClient.deleteResumableUpload(token.getToken(), uploadId));
     }
