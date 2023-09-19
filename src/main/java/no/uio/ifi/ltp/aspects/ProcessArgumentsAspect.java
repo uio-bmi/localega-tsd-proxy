6f352e776807a181c8c96b9cc82a314c48d63a74
Parisa Tejari <parisa.tejari@uib.no>
2023-09-19 13:37:08 +0200

    update spring boot version with the necessary changes

--- src/main/java/no/uio/ifi/ltp/aspects/ProcessArgumentsAspect.java
+++ src/main/java/no/uio/ifi/ltp/aspects/ProcessArgumentsAspect.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 /**
  * AOP aspect that processes HTTP request parameters.
