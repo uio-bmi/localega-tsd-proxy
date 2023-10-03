61fe8406eab7e6e3f11b22ae131e6690bc63fa85
Parisa Tejari <parisa.tejari@uib.no>
2023-10-03 10:48:25 +0200

    add oauth url to security matcher

--- src/main/java/no/uio/ifi/ltp/LocalEGATSDProxyApplication.java
+++ src/main/java/no/uio/ifi/ltp/LocalEGATSDProxyApplication.java
@@ -64,7 +64,7 @@ public class LocalEGATSDProxyApplication {
                 requiresChannel(channel -> channel.anyRequest().requiresSecure())
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(entryPoint))
                 .csrf(AbstractHttpConfigurer::disable)
-                .securityMatcher("/token.html", "/token", "/user").authorizeHttpRequests(request -> request
+                .securityMatcher("/token.html", "/token", "/user", "/oauth2/authorization/elixir-aai").authorizeHttpRequests(request -> request
                         .requestMatchers("/token.html").authenticated()
                         .requestMatchers("/token").authenticated()
                         .requestMatchers("/user").authenticated())
